f1a0ee9dda3f0165a560d5b5801df4158aa4edff
jlbuild <juliabuildbot@gmail.com>
2026-01-24 17:30:14 +0000

    libaec_jll build 1.1.5+0

--- src/libaec_jll.jl
+++ src/libaec_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("libaec")
-JLLWrappers.@generate_main_file("libaec", UUID("477f73a3-ac25-53e9-8cc3-50b2fa2566f0"))
+JLLWrappers.@generate_main_file("libaec", Base.UUID("477f73a3-ac25-53e9-8cc3-50b2fa2566f0"))
 end  # module libaec_jll
